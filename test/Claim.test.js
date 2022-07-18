b449a2bd154780e639f501e5dc67f8505756a5d5
BillyOriginals <billy.originals@gmail.com>
2022-07-18 19:48:40 +0300

    changes

--- test/Claim.test.js
+++ test/Claim.test.js
@@ -22,61 +22,65 @@ describe("Claim Test", async () => {
         );
         claim = await claimFactory.deploy(100, "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E"); // set initial GRAD price as $0.01
 
-        dai.mint(investor, 100000 * 1e18);
-        dai.connect(investor).approve(claim.address, 100000 * 1e18);
+        // dai.mint(investor, 100000 * 1e18);
+        // dai.connect(investor).approve(claim.address, 100000 * 1e18);
     });
 
-    it("get share", async () => {
-        const percent_ = await claim.getShare(
-            BigNumber(7 * 1e5 * 1e9).toString(),
-            BigNumber(70 * 1e6 * 1e9).toString(),
-            BigNumber(5 * 1e4).toString()
-        );
-        expect(percent_).to.equal(5 * 1e2);
+    describe("Claim basic functions", async () => { 
+        it("get share", async () => {
+            const percent_ = await claim.getShare(
+                BigNumber(7 * 1e5 * 1e9).toString(),
+                BigNumber(70 * 1e6 * 1e9).toString(),
+                BigNumber(5 * 1e4).toString()
+            );
+            expect(percent_).to.equal(5 * 1e2);
+        });
+    
+        it("change Grad Price", async () => {
+            const currentPrice = await claim.gradPrice();
+            await claim.changeGradPrice(1000);
+            const newGradPrice = await claim.gradPrice();
+            expect(newGradPrice - currentPrice).to.equal(900);
+        });
+    
+        it("change payment token", async () => {
+            const currentPaymentToken = await claim.paymentToken();
+            expect(currentPaymentToken).to.equal(dai.address);
+            usdc = await ethers.getContractAt(
+                "MockERC20",
+                "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
+            );
+            await claim.changePaymentToken(usdc.address);
+            const newPaymentToken = await claim.paymentToken();
+            expect(newPaymentToken).to.equal(usdc.address);
+        });
+    
+        it("toggle status", async () => {
+            const currentStatus = await claim.saleOpened();
+            expect(currentStatus).to.equal(false);
+            
+            await claim.toggleSaleStatus();
+    
+            const newStatus = await claim.saleOpened();
+            expect(newStatus).to.equal(true);
+        });
+    
+        it("add investor to whitelist", async () => {
+            const currentAmountOfTokens = await claim.saleInvestorWhitelist(investor.address);
+            expect(currentAmountOfTokens).to.equal(0);
+    
+            await claim.setAddressToInvestorWhitelist(investor.address, 1 * 1e6 * 1e9);
+    
+            const NewAmountOfTokens = await claim.saleInvestorWhitelist(investor.address);
+            expect(NewAmountOfTokens).to.equal(1 * 1e6 * 1e9);
+        });
     });
 
-    it("change Grad Price", async () => {
-        const currentPrice = await claim.gradPrice();
-        await claim.changeGradPrice(1000);
-        const newGradPrice = await claim.gradPrice();
-        expect(newGradPrice - currentPrice).to.equal(900);
+    describe("Claim setTerm", async () => { 
+        it("change Grad Price", async () => {
+            console.log(await claim.terms(investor.address));
+        });
     });
-
-    it("change payment token", async () => {
-        const currentPaymentToken = await claim.paymentToken();
-        expect(currentPaymentToken).to.equal(dai.address);
-        usdc = await ethers.getContractAt(
-            "MockERC20",
-            "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
-        );
-        await claim.changePaymentToken(usdc.address);
-        const newPaymentToken = await claim.paymentToken();
-        expect(newPaymentToken).to.equal(usdc.address);
-    });
-
-    it("toggle status", async () => {
-        const currentStatus = await claim.saleOpened();
-        expect(currentStatus).to.equal(false);
-        
-        await claim.toggleSaleStatus();
-
-        const newStatus = await claim.saleOpened();
-        expect(newStatus).to.equal(true);
-    });
-
-    it("add investor to whitelist", async () => {
-        const currentAmountOfTokens = await claim.saleInvestorWhitelist(investor.address);
-        expect(currentAmountOfTokens).to.equal(0);
-
-        await claim.setAddressToInvestorWhitelist(investor.address, 1 * 1e6 * 1e9);
-
-        const NewAmountOfTokens = await claim.saleInvestorWhitelist(investor.address);
-        expect(NewAmountOfTokens).to.equal(1 * 1e6 * 1e9);
-    });
-
-    // describe("Claim getShare", async () => { 
-        
-    // });
 
     // it("Cannot buy without sale is started", async () => {
     //     await expect(
